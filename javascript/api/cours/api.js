d796924b528cc1d8d818ed7cd1b2f2e72a40a890
marlener87 <53823616+marlener87@users.noreply.github.com>
2023-09-07 15:28:29 +0200

    ajout code api.js

--- javascript/api/cours/api.js
+++ javascript/api/cours/api.js
@@ -101,7 +101,7 @@ setTimeout(() => {
 
 // les promesses (promise), async, await
 //promise
-fetch("monlien").then((res) => res); // '.then((res) => res)' ce bout de code ne sera executé que lorsque 'fetch('monlien')' sera fait, lorsqu'on aura un retour, des données
+//fetch("monlien").then((res) => res); // '.then((res) => res)' ce bout de code ne sera executé que lorsque 'fetch('monlien')' sera fait, lorsqu'on aura un retour, des données
 
 // async/await
 function fetchData() {
@@ -127,3 +127,65 @@ const fetchData2 = async () => {
 
 //---------------------
 // LE JSON
+// format pour faire transiter des données
+// méthode .json() => méthode qui s'autorésout, donc on la met dans une promesse en renvoyant le body de la requête
+// on fait une requête de data.json et on les transforme de telle sorte que la réponse soit lisible
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // stringify => il convertit en json
+    //console.log(JSON.stringify(data)); // on transforme un objet en chaîne de caractères json
+    let settings = JSON.stringify(data);
+    //console.log(settings);
+    // méthode 'parse' => transforme json en objet js
+    console.log(JSON.parse(settings));
+  });
+
+//-----------------
+// web API
+// API qu'on a de base dans le navigateur (geolocation, canva, history, localstorage)
+
+//----------------------------------------------------------------------------------------------------
+// CLIENT STORAGE
+
+//-------------------
+// localStorage (objet de 'window)
+localStorage.data = "Je stocke la data dans le localStorage";
+// pour récupérer la data
+console.log(localStorage.data);
+document.body.textContent = localStorage.data;
+// pour supprimer l'éleemnt du localStorage
+localStorage.removeItem("data");
+
+// on ne peut pas passer un objet, one ne peut pas passer un tableau ; il faut passer une chaîne de caractère
+const obj = {
+  name: "Denis",
+  age: 22,
+};
+//localStorage.user = obj; // il ne saura pas l'interpréter, car ça n'est pas des chaînes de caractères. il faut utiliser une méthode pour le mettre en chaîne de caractères
+localStorage.user = JSON.stringify(obj);
+console.log(localStorage.user);
+console.log(JSON.parse(localStorage.user));
+
+// pour mettre à jour un localStorage
+//localStorage.user = "Denis";
+
+//-----------------
+// Session Storage
+// données stockées pendant la durée de la session de l'utilisateur, quand la page est fermée, la session est terminée. Pour stocker des choses provisoirement, de façon courte
+sessionStorage.dataSettings = "55px";
+console.log(sessionStorage.dataSettings);
+// pour supprimer la session storage
+sessionStorage.clear();
+
+//----------------
+// Cookies
+// plutôt utilisé en back
+// qch qui est envoyé d'un serveur vers un navigateur, puis le cookie est stocké par le navigateur. Tous les cookies qui sont stockés sont tjs envoyés, ce qui permet de vérifier que l'on est bien la bonne personne, si on est bien connecté, souvent utilisé comme un token
+console.log(document.cookie);
+
+// pour ajouter un cookie
+document.cookie = "username=FromScratch";
+// si on veut que le cookie reste plus longtemps que la session
+document.cookie = "pseudo=FS; path=/; max-age=450000; secure; samesite";
+// ou à la place de max-age, mettre expires=Thu, 31 Dec 2099 23:59:59 GMT
